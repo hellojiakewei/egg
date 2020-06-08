b36507c6022f360d47a81e5f9c4984d929917da9
贾克伟 <>
2020-06-08 22:59:35 +0800

    no message

--- app/model/user.js
+++ app/model/user.js
@@ -1,21 +1,20 @@
-module.exports =  function (app) {
+module.exports = function (app) {
     const mongoose = app.mongoose;
     const Schema = mongoose.Schema;
 
     const UserSchema = new Schema({
-        record_id: { type: String,required:true },
-        username: { type: String,required: true },
-        password: { type: String,required: true },
-        email: { type: String,required: true },
-        age:{type:Number,required:true,min:6,max:500},
-        phone: {type: String, unique:true,required: [true, 'User phone number required']
+        record_id: {type: String, required: true},
+        username: {type: String, required: true},
+        password: {type: String, required: true},
+        email: {type: String, required: true},
+        age: {type: Number, required: true, min: 6, max: 500},
+        phone: {
+            type: String, unique: true, required: [true, 'User phone number required']
         },
-        date:{type:Date,default:Date.now()}
+        date: {type: Date, default: Date.now()}
     });
-    UserSchema.statics={
-        findUserByPhone: function(phone) {
-            return this.find({phone: phone})
-        }
-    }
-    return mongoose.model('User', UserSchema,'user')
+    UserSchema.statics.findUserByPhone = function (phone) {
+        return this.find({phone: phone})
+    };
+    return mongoose.model('User', UserSchema, 'user')
 };
